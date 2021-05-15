cf06dad54a313f990ad87e520eef8860227b83c9
Sahibpreet Kaur <sahibkaur81@gmail.com>
2021-05-15 23:26:10 +0530

    adds key in quizCategory

--- src/App.js
+++ src/App.js
@@ -2,14 +2,39 @@ import QuizCategory from './QuizCategory'
 import './App.css';
 
 function App() {
-  const categories = ['Science', 'Tech', 'Plants', 'English', 'History', 'Economics']
+  const categories = [
+    {
+      id: 0,
+      title: 'Science'
+    },
+    {
+      id: 1,
+      title: 'Tech'
+    },
+    {
+      id:2,
+      title:'Plants'
+    },
+    {
+      id:3,
+      title:'English'
+    },
+    {
+      id:4,
+      title:'History'
+    },
+    {
+      id:5,
+      title:'Economics'
+    }
+  ];
 
   return (
     <div className="App">
       <h1>Quiz App</h1>
       <div className="categories">
         {categories.map(item => 
-        <QuizCategory title={item}></QuizCategory>
+        <QuizCategory title={item.title} key={item.id}></QuizCategory>
         )}
       </div>
     </div>
